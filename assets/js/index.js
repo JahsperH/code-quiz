b951c40dcbbfaefc8642ba3dd985addea7b6dd0f
JahsperH <Jahsper.harrell@gmail.com>
2023-04-25 22:56:38 -0600

    Added another html page and started working on the JS.

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+var strtQuiz = document.getElementById("startQuiz");
+
+var qArray = [
+    "How do you create a function in JavaScript?",
+    "How do you call a function named 'myFunction'?",
+    "How do you write an IF statement in JavaScript?",
+    "How do you write a header in HTML?",
+];
+
+var corArray = [
+    "function myFunction()",
+    "myFunction()",
+    "if (i == 5)",
+    "<h1>",
+];
+
+var wroArray = [
+    ["function = myFunction()", "function myFunction()", "function:myFunction()"],
+    ["call myFunction()", "call function myFunction()", "myFunction()"],
+    ["if i = 5 then", "if i == 5 then", "if i = 5"],
+    ["<header>", "<head>", "<p>"],
+];
--- assets/js/style.js
+++
@@ -1,1 +0,0 @@
-var strtQuiz = document.getElementById("startQuiz");
